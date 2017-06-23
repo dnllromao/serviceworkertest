7e48733cacf9beecfdad8fbaeafced9153946f1e
dnllromao <dnllromao@gmail.com>
2017-06-23 15:52:22 +0200

    event on install

--- site.js
+++ site.js
@@ -1,5 +1,7 @@
 console.log(navigator);
 
+var version = 'v1::';
+
 if ('serviceWorker' in navigator) {
 
   console.log('CLIENT: service worker registration in progress.');
@@ -14,3 +16,33 @@ if ('serviceWorker' in navigator) {
 } else {
   console.log('CLIENT: service worker is not supported.');
 }
+
+self.addEventListener("install", function(event) {
+  console.log('WORKER: install event in progress.');
+  console.log(event);
+  event.waitUntil(
+    /* The caches built-in is a promise-based API that helps you cache responses,
+       as well as finding and deleting them.
+    */
+    console.log(caches);
+    caches
+      /* You can open a cache by name, and this method returns a promise. We use
+         a versioned cache name here so that we can remove old cache entries in
+         one fell swoop later, when phasing out an older service worker.
+      */
+      .open(version + 'fundamentals')
+      .then(function(cache) {
+      	console.log(cache);
+        /* After the cache is opened, we can fill it with the offline fundamentals.
+           The method below will add all resources we've indicated to the cache,
+           after making HTTP requests for each of them.
+        */
+        return cache.addAll([
+          '/'
+        ]);
+      })
+      .then(function() {
+        console.log('WORKER: install completed');
+      })
+  );
+});
