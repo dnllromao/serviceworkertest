b08558c4b8f61fd3f66be05732386d7af3ca0b64
dnllromao <dnllromao@gmail.com>
2017-06-23 16:35:52 +0200

    bof

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,31 @@
-console.log(navigator);
+var version = 'v1::';
 
-if ('serviceWorker' in navigator) {
-
-  console.log('CLIENT: service worker registration in progress.');
-
-  navigator.serviceWorker.register('https://serviceworker.herokuapp.com/service-worker.js')
-  	.then(function() {
-	    console.log('CLIENT: service worker registration complete.');
-	  }, function() {
-	    console.log('CLIENT: service worker registration failure.');
-	 });
-
-} else {
-  console.log('CLIENT: service worker is not supported.');
-}
+self.addEventListener("install", function(event) {
+  console.log('WORKER: install event in progress.');
+  console.log(event);
+  event.waitUntil(
+    /* The caches built-in is a promise-based API that helps you cache responses,
+       as well as finding and deleting them.
+    */
+    caches
+      /* You can open a cache by name, and this method returns a promise. We use
+         a versioned cache name here so that we can remove old cache entries in
+         one fell swoop later, when phasing out an older service worker.
+      */
+      .open(version + 'fundamentals')
+      .then(function(cache) {
+      	console.log(cache);
+      	console.log('reste reveillée');
+        /* After the cache is opened, we can fill it with the offline fundamentals.
+           The method below will add all resources we've indicated to the cache,
+           after making HTTP requests for each of them.
+        */
+        return cache.addAll([
+          '/'
+        ]);
+      })
+      .then(function() {
+        console.log('WORKER: install completed');
+      })
+  );
+});
